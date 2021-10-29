b72896f0b8cc9cef38e25ff78c4b6ad28a9033d1
Austin Emmanuval Noble <72311538+austinemble@users.noreply.github.com>
2021-10-30 00:05:52 +0530

    Add files via upload

---
+++ src/urlConfig.js
@@ -0,0 +1,8 @@
+const baseUrl = "https://nxt-gen-backend.herokuapp.com";
+//const baseUrl = "http://localhost:2000";
+
+export const api = `${baseUrl}/api`;
+
+export const generatePublicUrl = (fileName) => {
+  return `${baseUrl}/public/${fileName}`;
+};
